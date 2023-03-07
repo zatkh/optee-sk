3a60367d58a94517bd70ee1148ab136f660d65f7
Thara Gopinath <tgopinath@microsoft.com>
2023-03-07 18:36:38 +0000

    Merged PR 18: Add script to build optee for x64.
    Script to set up optee environment for x64.
    
    source build_vsm_optee.sh   <- need to run only once for a terminal session
    
    To build optee for x64:
    make
    
    To clean:
    make clean
    
    Signed-off-by: ztarkhani <ztarkhani@microsoft.com>
    Signed-off-by: Thara Gopinath <tgopinath@microsoft.com>

---
+++ build_vsm_optee.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+################################################################################
+# Copyright (c) 2022 Microsoft Corporation
+################################################################################
+
+export TOP_DIR=$1
+export INSTALL_DEPENDENCY=$2
+export ARCH=x64
+export CC=x86_64-linux-gnu-gcc
+export CXX=x86_64-linux-gnu-gcc-g++
+export TA_DEV_KIT_ROOT=$TOP_DIR/
+export GCC_TOOLCHAIN_PATH=/usr/bin/
+export CROSS_COMPILE=$GCC_TOOLCHAIN_PATH/x86_64-linux-gnu-
+export CROSS_COMPILE64=$GCC_TOOLCHAIN_PATH/x86_64-linux-gnu-
+export  O=$TOP_DIR/out/x64-plat-x86_64
+export  CFG_TEE_BENCHMARK=n
+export  CFG_TEE_CORE_LOG_LEVEL=1
+#export  CFG_WITH_USER_TA=n
+export  DEBUG=1
+export O=./out
+
+if $INSTALL_DEPENDENCY
+then
+    echo "install some dependencies"
+    pip3 install pycrypto
+    pip3 install pyelftools
+    pip3 install pycryptodomex
+
+fi
